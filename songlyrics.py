2e3fecbcddf95985af7123a3eac8a8de2cfbc4b6
JoakBouy <B.joak@alustudent.com>
2023-01-27 10:55:14 +0300

    song lyrics

---
+++ songlyrics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import re
+
+#for song lyrics
+text = "[Verse 1] some lyrics"
+
+match = re.search(r"\[Verse (\d)\] (.*)", text)
+
+if match:
+    print(match.group(1))  # 1
+    print(match.group(2))  # some lyrics
+else:
+    print("No match found")
